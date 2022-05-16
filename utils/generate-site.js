f3690db1be3c46702f9ae960e6d95133414977a3
Paul Haberle <61989020+phaberle@users.noreply.github.com>
2022-05-16 14:38:57 -0500

    last minute changes to generate-site.js

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -1,19 +1,19 @@
 const fs = require('fs');
-const writeFile = fileCOntent => {
+const writeFile = fileContent => {
     return new Promise((resolve, reject) => {
-        //fu there is an error, reject the promise and send the error to the promise catch() method
-        if (err) {
+        fs.writeFile('./dist/index.html', fileContent, err => {
+          if (err) {
             reject(err);
-            //return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
             return;
-        }
-        //if everything went well, resolve the Promise and send the successful data to the .then() method
-        resolve({
+          }
+    
+          resolve({
             ok: true,
             message: 'File created!'
+          });
         });
-    });
-};
+      });
+    };
 
 // copying file
 const copyFile = () => {
